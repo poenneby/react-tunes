e3556853f7fe7f56c7f093602b50d433367458df
Peter Onneby <ponneby@xebia.fr>
2017-11-16 22:12:34 +0100

    Introduce delay to avoid sound clipping

--- src/Track.js
+++ src/Track.js
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
 
+const START_DELAY = 0.3;
+
 export default class Track extends Component {
   render() {
     const {notesPerQuarter} = this.props;
@@ -7,7 +9,7 @@ export default class Track extends Component {
     const notesPerBar = quartersPerMinute * notesPerQuarter;
     const notesPerSection = 4 * notesPerBar;
     const childrenWithProps = React.Children.map(this.props.children, (component, index) => {
-      const sectionStartTime = notesPerSection * index;
+      const sectionStartTime = START_DELAY + notesPerSection * index;
       return React.cloneElement(component, {sectionStartTime, quartersPerMinute, notesPerQuarter});
     });
     return <span className="Track">{this.props.trackName}: {childrenWithProps}</span>;
